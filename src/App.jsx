5aaf3b0fb720f2e1569fa4214477aab5ec4952f6
Khushi Gupta <148431685+khushigupta345@users.noreply.github.com>
2025-08-30 22:03:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import { Route, Routes } from "react-router-dom"
-import Login from "./component/login"
+/*import Login from "./component/login"
 import SignupPage from "./component/SignupPage"
-import { Home } from "./component/home"
+import { Home } from "./component/home"*/
 
-
+import BulkOrderForm2 from "./component/BulkOrderForm2"
 function App() {
   
 
   return (
     <>
  
-
-     <Routes>
-      <Route path="/login" element={<Login/>} />
-
-    <Route path="/" element={<Home/>} />
-
-     </Routes>
-       <Routes>
-      <Route path="/signup" element={<SignupPage/>} />
-
-    
-     </Routes>
-    </>
+<BulkOrderForm2 />
   )
 }
 
