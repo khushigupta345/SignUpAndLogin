3ee19fff84b6fdc493a9cbfb7749c80199dd4138
Khushi Gupta <148431685+khushigupta345@users.noreply.github.com>
2025-09-04 20:39:35 +0530

    Update BulkOrderForm3.jsx

--- src/component/BulkOrderForm3.jsx
+++ src/component/BulkOrderForm3.jsx
@@ -510,7 +510,7 @@ setFormData(initialFormData);
      type="text"
       name="email"
       required
-      value={formData.eamil}
+      value={formData.email}
       onChange={handleChange}
         pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$"
        className="flex-1 bg-transparent outline-none border-0 px-3 py-2 text-xs"
