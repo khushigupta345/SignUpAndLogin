3421114c98b525979ffd59e90cf286c192167a2a
Khushi Gupta <148431685+khushigupta345@users.noreply.github.com>
2025-09-06 15:13:47 +0530

    Update BulkOrderForm3.jsx

--- src/component/BulkOrderForm3.jsx
+++ src/component/BulkOrderForm3.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { LuUserRound } from "react-icons/lu";
+{/*import { LuUserRound } from "react-icons/lu";*/}
+
+import { UserRound } from "lucide-react";
 import toast, { Toaster } from "react-hot-toast";
 import { FiUpload, FiTrash2 } from "react-icons/fi";
 import { CiMail, CiMobile3 } from "react-icons/ci";
